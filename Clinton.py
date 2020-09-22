d51a9ad2412d8c3be4d89f7aaf0f3297ef85b73e
Machuki Clinton <48854561+Machukichuxx@users.noreply.github.com>
2020-09-22 12:36:53 +0300

    Update Clinton.py

--- Clinton.py
+++ Clinton.py
@@ -1,5 +1,5 @@
 FirstName = input("What is your First Name ?")
-SecondName = input("What is Second Name ?")
+SecondName = input("What is your Second Name ?")
 Admissionnumber = input("What is your admission Number")
 Email = "@Zetech.ac.ke"
 
