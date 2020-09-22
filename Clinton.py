ebb713a83e251e3cbd613469909923ff4e5689c1
Machukichuxx <machukichuxx@gmail.com>
2020-09-22 12:32:43 +0300

    Adding Files

---
+++ Clinton.py
@@ -0,0 +1,6 @@
+FirstName = input("What is your First Name ?")
+SecondName = input("What is Second Name ?")
+Admissionnumber = input("What is your admission Number")
+Email = "@Zetech.ac.ke"
+
+print("Your email is " + SecondName + FirstName + Admissionnumber + Email)
